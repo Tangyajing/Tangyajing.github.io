8d145cd7fb1980507ad570a672b9f81d6124aec3
Tangyajing <1191995120@qq.com>
2016-11-21 21:30:57 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	$("#content .confirm button").click(function(){
+		var sexValue = $(".sex input[name='sex']:checked").val();
+		var username = $(".username input[name='username']").val();
+
+		if(username && sexValue){
+			$.getJSON('./text.json',function(data){
+				var showData = [];
+				if(sexValue == 'man'){  
+					showData = data.common.concat(data.man);
+				}
+				if (sexValue == "woman"){
+					showData = data.common.concat(data.woman);
+				}
+				
+				var dataLength = showData.length;
+				var randonNum = Math.round(Math.random()*(dataLength-1));
+				var theText = showData[randonNum];
+
+				var lastText = setText(theText,username);
+
+				$('#header .welcome').text('请长按保存您的小故事');
+				$('#header img').remove();
+				$('body').children('#content').remove();
+				$('body').append('<div id="textContent">'+lastText+'</div>');
+
+				
+				html2canvas($("body #textContent"), {
+                	onrendered: function(canvas) {
+                	    var theImage = new Image();
+                	    theImage.src = canvas.toDataURL("image/png"); 
+                	    theImage.id = 'lastImg'; 
+                	    $('body').append(theImage);
+                	    $('body').children('#textContent').remove();
+
+                	}
+
+				});
+
+				
+
+			})
+		}
+	})
+})
+
+function setText(text,user){
+	var textHtml = '';
+	var replaceHtml = '<span class="bolder">'+user+'</span>';
+	for (var i = 0; i < text.length; i++) {
+
+		text[i] = text[i].replace(/<xx>/g,replaceHtml);
+		textHtml += "<p>"+text[i]+"</p>";
+	}
+	return textHtml;
+}
